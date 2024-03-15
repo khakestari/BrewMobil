c866235f00bf0f244cd49cd70a6b666d33817a83
khakestari <mostafanmz@yahoo.com>
2024-03-15 23:31:40 +0330

    bind Login ViewModel to Login View

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './login_viewmodel.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -8,8 +9,33 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  LoginViewModel _viewModel = LoginViewModel(null);
+
+  TextEditingController _userNameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  _bind() {
+    _viewModel.start();
+    _userNameController
+        .addListener(() => _viewModel.setUserName(_userNameController.text));
+    _passwordController
+        .addListener(() => _viewModel.setPassword(_passwordController.text));
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
   }
 }
--- lib/presentation/login/login_viewmodel.dart
+++ lib/presentation/login/login_viewmodel.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
+import 'package:advanced_shop_app/domain/usecase/login_usecase.dart';
+
 import '../base/baseviewmodel.dart';
+import '../common/freezed_data_classes.dart';
 
 class LoginViewModel extends BaseViewModel
     implements LoginViewModelInputs, LoginViewModelOutputs {
@@ -8,9 +11,11 @@ class LoginViewModel extends BaseViewModel
       StreamController<String>.broadcast();
   StreamController _passwordStreamController =
       StreamController<String>.broadcast();
-  
-  var loginObject = LoginObject("","");
-  
+
+  var loginObject = LoginObject("", "");
+
+  LoginUseCase? _loginUseCase;
+  LoginViewModel(this._loginUseCase);
   // inputs
   @override
   void dispose() {
@@ -30,19 +35,32 @@ class LoginViewModel extends BaseViewModel
   Sink get inputUserName => _userNameStreamController.sink;
 
   @override
-  login() {
-    // TODO: implement login
-    throw UnimplementedError();
+  login() async {
+    (await _loginUseCase.execute(
+            LoginUseCaseInput(loginObject.userName, loginObject.password)))
+        .fold(
+            (failure) => {
+                  // left -> failure
+                  print(failure.message)
+                },
+            (data) => {
+                  // right -> success (data)
+                  print(data.customer?.name)
+                });
   }
 
   @override
   setPassword(String password) {
     inputPassword.add(password);
+    loginObject = loginObject.copyWith(
+        password: password); // data class opration same as kotlin
   }
 
   @override
   setUserName(String userName) {
     inputUserName.add(userName);
+    loginObject = loginObject.copyWith(
+        userName: userName); // data class opration same as kotlin
   }
 
   // outputs
